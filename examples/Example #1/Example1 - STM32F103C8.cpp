70fb744d0035bd7a1f8a18f1a729aaa569c437db
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2023-11-26 22:49:30 +0100

    Added lib example.

---
+++ examples/Example #1/Example1 - STM32F103C8.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file Main.cpp
+ * @author silvio3105 (www.github.com/silvio3105)
+ * @brief Main source file.
+ * 
+ * @copyright Copyright (c) 2023, silvio3105
+ * 
+ */
+
+/*
+	Copyright (c) 2023, silvio3105 (www.github.com/silvio3105)
+
+	Access and use of this Project and its contents are granted free of charge to any Person.
+	The Person is allowed to copy, modify and use The Project and its contents only for non-commercial use.
+	Commercial use of this Project and its contents is prohibited.
+	Modifying this License and/or sublicensing is prohibited.
+
+	THE PROJECT AND ITS CONTENT ARE PROVIDED "AS IS" WITH ALL FAULTS AND WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+	THE AUTHOR KEEPS ALL RIGHTS TO CHANGE OR REMOVE THE CONTENTS OF THIS PROJECT WITHOUT PREVIOUS NOTICE.
+	THE AUTHOR IS NOT RESPONSIBLE FOR DAMAGE OF ANY KIND OR LIABILITY CAUSED BY USING THE CONTENTS OF THIS PROJECT.
+
+	This License shall be included in all functional textual files.
+*/
+
+
+// ----- INCLUDE FILES
+#include 			"Main.hpp"
+#include			"sBuildInfo.h"
+
+// Build info (will be placed at 0x10C, right after vector table)
+__SBI("SBI_FW", "v1.13.08rc5", "BluePill", "E");
+
+
+// ----- STATIC FUNCTION DECLARATIONS
+static void sysClockInit(void);
+
+
+// ----- APP ENTRY
+int main(void)
+{
+	HAL_Init();
+	sysClockInit();
+
+	// Just to make sure compiler does not remove build info
+	SBI_USED;
+
+	// Print example:
+	// printf("FW version %s\nBuild date %s\n", SBI_VER, SBI_DATE);
+
+	while (1);
+}
+
+
+// ----- STATIC FUNCTION DEFINITIONS
+static void sysClockInit(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+															|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+}
+
+
+// END WITH NEW LINE
